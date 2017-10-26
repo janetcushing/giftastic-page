68e28a6ccd267ffc76ba072ac9c9c34a36650ca3
janetcushing <janet.cushing@gmail.com>
2017-10-26 10:01:32 -0400

    updated files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,8 +5,10 @@
 //-------------------------//
 // global variables
 //-------------------------//
-var dances = ["disco", "hip hop", "macarena", "polka", "flash mob"];
+var topics = ["disco", "hip hop", "macarena", "polka", "flash mob"];
 var result = [];
+var gifUrlStill = "";
+var gifUrlAnimate = "";
 
 //-------------------------//
 // functions
@@ -14,16 +16,16 @@ var result = [];
 
 //-------------------------------------------------------------//
 // add the buttons to the dom
- // dynamicaly generate a button for each dance in the array
+// dynamicaly generate a button for each dance in the array
 //-------------------------------------------------------------//
 function renderButtons() {
   $("#dance-view").empty();
-  for (let i = 0; i < dances.length; i++) {
+  for (let i = 0; i < topics.length; i++) {
     let newButton = $("<button>");
     newButton.addClass("dance");
-    newButton.attr("data-name", dances[i]);
-    newButton.attr("value", dances[i]);
-    newButton.text(dances[i]);
+    newButton.attr("data-name", topics[i]);
+    newButton.attr("value", topics[i]);
+    newButton.text(topics[i]);
     // Adding the button to the HTML
     $("#dance-view").append(newButton);
     $("#dance-input").val("");
@@ -33,12 +35,12 @@ function renderButtons() {
 
 //---------------------------------------------------------------//
 // handle submit button click events 
-// add the dance from the text field to the dances array
+// add the dance from the text field to the topics array
 //---------------------------------------------------------------//
 function addNewButton(dance) {
   // event.preventDefault() prevents the form from trying to submit itself.
   event.preventDefault();
-  dances.push(dance);
+  topics.push(dance);
   renderButtons();
 }
 
@@ -46,7 +48,7 @@ function addNewButton(dance) {
 // query the giphy api to bring back dance images
 //---------------------------------------------------------------//
 function queryGiphy(danceName) {
-  let limit = 4;
+  let limit = 10;
   let rating = "g";
   let apiKey = "2fLgdLgXeV1kUnyRwYKej42K3Dtjue87";
   let queryURL = "https://api.giphy.com/v1/gifs/search";
@@ -61,23 +63,42 @@ function queryGiphy(danceName) {
     method: 'GET'
   }).done(function (response) {
     result = response.data;
+    // console.log(result);
+    $("#animateMsg").text("   Click on the image to turn animation on and off!");
     let rowDiv = $("<div>");
     rowDiv.addClass("row");
     rowDiv.attr("id", "gifReturnRow");
     $("#gifReturnDiv").append(rowDiv);
 
     for (let i = 0; i < result.length; i++) {
-      let gifUrl = result[i].images.fixed_height.url;
+      gifUrlStill = result[i].images.fixed_height_still.url;
+      gifUrlAnimate = result[i].images.fixed_height.url;
+      let title = result[i].title;
+      let state = "still";
       let rating1 = result[i].rating;
       let gifDiv = $("<div>");
+      if (i === 0) {
+        let marginDiv = $("<div>");
+        marginDiv.addClass("col-md-3");
+        $("#gifReturnRow").prepend(marginDiv);
+      }
       gifDiv.addClass("col-md-3");
       let img1 = $("<img class=giphyGif>");
-      img1.attr("src", gifUrl);
-      let p1 = $("<p>");
-      p1.text("Rating: " + rating1);
+      img1.attr("data-still", gifUrlStill);
+      img1.attr("data-animate", gifUrlAnimate);
+      img1.attr("data-state", state);
+      img1.attr("src", gifUrlStill);
+      img1.attr("alt", title);
+      let p2 = $("<p>");
+      p2.text("Rating: " + rating1);
       gifDiv.append(img1);
-      gifDiv.append(p1);
+      gifDiv.append(p2);
       $("#gifReturnRow").prepend(gifDiv);
+      if (i === 1) {
+        let marginDiv = $("<div>");
+        marginDiv.addClass("col-md-3");
+        $("#gifReturnRow").prepend(marginDiv);
+      }
     }
   });
 }
@@ -112,8 +133,19 @@ $(document).ready(function () {
     }
   });
 
+  //------------------------------------------------------//
+  // Turn animation on and off by clicking the image
+  //------------------------------------------------------//
+  $('#gifReturnDiv').on('click', '.giphyGif', function () {
+    var dataState = $(this).attr("data-state");
+    if (dataState === 'still') {
+      $(this).attr("src", $(this).attr("data-animate"));
+      $(this).attr("data-state", "animate");
+    } else if (dataState === 'animate') {
+      $(this).attr("src", $(this).attr("data-still"));
+      $(this).attr("data-state", "still");
+    }
 
-
-
+  });
 
 });
